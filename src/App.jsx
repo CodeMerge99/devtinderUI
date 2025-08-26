5387d8a9865e0feca42eed58dad3a2991b165fa6
Piyush Tirpude <piyushtirpude@gmail.com>
2025-08-26 21:25:45 +0530

    added Navbar to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
+import Navbar from "./components/Navbar"
 
 function App() {
   
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        hello
-      </h1>
+      <Navbar/>
     </>
   )
 }
